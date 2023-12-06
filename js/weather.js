48cae79cedea3750675c6fcadb3a90191f6d1d26
HAM!DREZA <hamidreza.eidy1999@gmail.com>
2023-12-06 13:36:20 +0330

    completed

--- js/weather.js
+++ js/weather.js
@@ -1,17 +1,20 @@
 class Weather {
     constructor(city, state) {
-        this.apiKey = '86b0bd9b76517148d71f0967cc7de574',
-            this.city = city,
+        this.apiKey = '86b0bd9b76517148d71f0967cc7de574'
+        this.city = city,
             this.state = state
     }
+
     async getWeather() {
         const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=${this.apiKey}`)
+
         if (response.ok) {
             const responseData = await response.json();
             return responseData;
         } else {
-            throw Error(response.status)
+            throw Error(response.status);
         }
+
     }
 
     changeLocation(city, state) {
@@ -19,8 +22,7 @@ class Weather {
             this.state = state
     }
 
-    get Location() {
-        return this.state + ' , ' + this.state;
+    get location() {
+        return this.state + ' , ' + this.city;
     }
-
 }
